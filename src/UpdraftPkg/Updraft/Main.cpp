cb328ae86553980e0eb4f4a8f293e50f76244ebd
prince0203 <mc.prince.0203@gmail.com>
2017-12-24 11:36:51 +0900

    Update Main.cpp

--- src/UpdraftPkg/Updraft/Main.cpp
+++ src/UpdraftPkg/Updraft/Main.cpp
@@ -9,15 +9,13 @@ extern "C" {
 EFI_STATUS UefiMain(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
   UefiSystem::initialize(SystemTable);
+  Logger::Println_("Hello, UEFI World!");
 
   // Check screen resolution
   Logger::Println_("Screen resolution: ", Screen::size());
 
   // Graphics test
-  constexpr Rect rect(200, 300, 123, 321);
-  constexpr Color color = Palette::Orange;
-  Logger::Println_("Draw a rectangle! ", rect, " Color: ", color);
-  rect.draw(color);
+  Rect(300, 200, 120, 230).draw(Palette::Orange);
   Circle(200, 300, 125).draw(Palette::Skyblue);
 
   UefiSystem::loop_forever();

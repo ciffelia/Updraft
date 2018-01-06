b6ed8f4cb79a84177ee018e371f7d253beb1d9a8
prince0203 <mc.prince.0203@gmail.com>
2018-01-06 20:00:37 +0900

    Refactor Circle.cpp

--- src/UpdraftPkg/Updraft/Graphics/Circle.cpp
+++ src/UpdraftPkg/Updraft/Graphics/Circle.cpp
@@ -4,9 +4,9 @@
 
 void Circle::draw(const Color color) const
 {
-  for(int32 y = center.y - r; y < center.y + r; y++)
+  for(int32 y = center.y - r; y < center.y + (int32)r; y++)
   {
-    for(int32 x = center.x - r; x < center.x + r; x++)
+    for(int32 x = center.x - r; x < center.x + (int32)r; x++)
     {
       if(Point(x, y).distanceFrom(center) <= r) Point(x, y).draw(color);
     }

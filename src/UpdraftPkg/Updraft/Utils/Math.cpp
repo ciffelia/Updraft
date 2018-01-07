a8f80813ccf3d2a63522c78271db836118651fd9
prince0203 <mc.prince.0203@gmail.com>
2018-01-07 20:39:19 +0900

    Add math functions and constants

--- src/UpdraftPkg/Updraft/Utils/Math.cpp
+++ src/UpdraftPkg/Updraft/Utils/Math.cpp
@@ -16,3 +16,38 @@ double Math::Sqrt(const double x)
 {
   return sqrt(x);
 }
+
+double Math::Sin(const double x)
+{
+  return sin(x);
+}
+
+double Math::Cos(const double x)
+{
+  return cos(x);
+}
+
+double Math::Tan(const double x)
+{
+  return tan(x);
+}
+
+double Math::Asin(const double x)
+{
+  return asin(x);
+}
+
+double Math::Acos(const double x)
+{
+  return acos(x);
+}
+
+double Math::Atan(const double x)
+{
+  return atan(x);
+}
+
+double Math::Atan2(const double y, const double x)
+{
+  return atan2(y, x);
+}
--- src/UpdraftPkg/Updraft/Utils/Math.hpp
+++ src/UpdraftPkg/Updraft/Utils/Math.hpp
@@ -1,9 +1,27 @@
 #pragma once
 
 namespace Math {
+  constexpr double Pi = 3.1415926535897932385;
+
+  constexpr double TwoPi = Pi * 2.0;
+
   double Pow(const double x, const double y);
 
   double Square(const double x);
 
   double Sqrt(const double x);
+
+  double Sin(const double x);
+
+  double Cos(const double x);
+
+  double Tan(const double x);
+
+  double Asin(const double x);
+
+  double Acos(const double x);
+
+  double Atan(const double x);
+
+  double Atan2(const double y, const double x);
 }

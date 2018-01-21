dae2e3eb9e7e7ce7e014cad55af19af36e1937a9
prince0203 <mc.prince.0203@gmail.com>
2018-01-21 14:15:45 +0900

    Add Line class

---
+++ src/UpdraftPkg/Updraft/Graphics/Line.cpp
@@ -0,0 +1,45 @@
+#include "Line.hpp"
+
+#include "../Utils/Utility.hpp"
+
+// Bresenham's line algorithm: https://ja.wikipedia.org/wiki/ブレゼンハムのアルゴリズム
+void Line::draw(const Color color) const
+{
+  int x0 = static_cast<int>(begin.x), y0 = static_cast<int>(begin.y),
+      x1 = static_cast<int>(end.x), y1 = static_cast<int>(end.y);
+
+  const bool steep = Math::Abs(y1 - y0) > Math::Abs(x1 - x0);
+
+  if (steep)
+  {
+    Swap(x0, y0);
+    Swap(x1, y1);
+  }
+  if (x0 > x1)
+  {
+    Swap(x0, x1);
+    Swap(y0, y1);
+  }
+
+  const int deltax = x1 - x0, deltay = Math::Abs(y1 - y0);
+  int error = deltax / 2;
+
+  const int ystep = (y0 < y1) ? 1 : -1;
+
+  int y = y0;
+
+  for (int x = x0; x <= x1; x++)
+  {
+    if (steep)
+      Point(y, x).draw(color);
+    else
+      Point(x, y).draw(color);
+    
+    error = error - deltay;
+    if (error < 0)
+    {
+      y += ystep;
+      error += deltax;
+    }
+  }
+}
---
+++ src/UpdraftPkg/Updraft/Graphics/Line.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "../Utils/Vec2.hpp"
+#include "../Utils/Math.hpp"
+
+struct Line
+{
+  Vec2 begin, end;
+
+  constexpr Line(const double x1, const double y1, const double x2, const double y2)
+    : begin(x1, y1)
+    , end(x2, y2)
+  { }
+
+  constexpr Line(const Vec2 _begin, const Vec2 _end)
+    : begin(_begin)
+    , end(_end)
+  { }
+
+  constexpr Line(const Vec2 _begin, const double x2, const double y2)
+    : begin(_begin)
+    , end(x2, y2)
+  { }
+
+  constexpr Line(const double x1, const double y1, const Vec2 _end)
+    : begin(x1, y1)
+    , end(_end)
+  { }
+
+  constexpr bool operator==(const Line &l) const
+  {
+    return begin == l.begin && end == l.end;
+  }
+
+  constexpr bool operator!=(const Line &l) const
+  {
+    return !(*this == l);
+  }
+
+  constexpr Vec2 vector() const
+  {
+    return end - begin;
+  }
+
+  double length() const
+  {
+    return begin.distanceFrom(end);
+  }
+
+  double distanceFrom(const Vec2 &p) const
+  {
+    const Vec2 ap = (p - begin);
+    const Vec2 ab = vector();
+
+    const double cross = (p - begin).cross(vector());
+
+    return Math::Abs(cross) / length();
+  }
+
+  void draw(const Color color) const;
+};

245f2060a02d1b48264499e6b5a989629359a73d
prince0203 <mc.prince.0203@gmail.com>
2018-01-28 11:54:54 +0900

    Remove Screen::Size(), Screen::Rect()

--- src/UpdraftPkg/Updraft/Graphics/Screen.cpp
+++ src/UpdraftPkg/Updraft/Graphics/Screen.cpp
@@ -2,19 +2,9 @@
 
 #include "../Graphics/Graphics.hpp"
 
-Size Screen::Size()
-{
-  return { Width(), Height() };
-}
-
 Point Screen::Center()
 {
   return { (int32)Width() / 2, (int32)Height() / 2 };
-}
-
-Rect Screen::Rect()
-{
-  return Rect::Rect({0, 0}, Size());
 }
 
 uint32 Screen::Width()
--- src/UpdraftPkg/Updraft/Graphics/Screen.hpp
+++ src/UpdraftPkg/Updraft/Graphics/Screen.hpp
@@ -2,15 +2,9 @@
 
 #include "../Utils/Types.hpp"
 #include "Point.hpp"
-#include "Rect.hpp"
-#include "Size.hpp"
 
 namespace Screen {
-  Size Size();
-
   Point Center();
-
-  Rect Rect();
 
   uint32 Width();
   uint32 Height();

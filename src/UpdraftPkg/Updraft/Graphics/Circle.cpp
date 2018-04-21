e9fb50df19b50261cc6f9b61d892abecddc04045
prince0203 <mc.prince.0203@gmail.com>
2018-04-21 23:01:14 +0900

    Fix Circle.cpp

--- src/UpdraftPkg/Updraft/Graphics/Circle.cpp
+++ src/UpdraftPkg/Updraft/Graphics/Circle.cpp
@@ -6,6 +6,7 @@
 Circle& Circle::moveBy(const Point v)
 {
   center += v;
+  return *this;
 }
 
 Circle Circle::movedBy(const Point v) const
@@ -15,14 +16,14 @@ Circle Circle::movedBy(const Point v) const
 
 void Circle::draw(const Color color) const
 {
-  if (center.x + r < 0 || center.y + r < 0)
+  if (center.x + (int)r < 0 || center.y + (int)r < 0)
     return;
-  if (center.x - r > (int)Screen::Width() || center.y - r > (int)Screen::Height())
+  if (center.x - (int)r > (int)Screen::Width() || center.y - (int)r > (int)Screen::Height())
     return;
 
-  for(int32 y = center.y - (int)r; y < center.y + (int32)r; y++)
+  for(int32 y = center.y - (int)r; y < center.y + (int)r; y++)
   {
-    for (int32 x = center.x - (int)r; x < center.x + (int32)r; x++)
+    for (int32 x = center.x - (int)r; x < center.x + (int)r; x++)
     {
       if(Point(x, y).distanceFrom(center) <= r) Point(x, y).draw(color);
     }
